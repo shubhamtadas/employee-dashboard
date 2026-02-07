3bd5d41f22b201fa6e98462bc1958f5734c48939
shutad6j <shubham.tadas.ext@siemens.com>
2026-02-07 17:30:49 +0530

    Fix naming

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css";
 
 import { ModuleRegistry, AllCommunityModule } from "ag-grid-community";
 
